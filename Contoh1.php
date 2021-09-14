4588c01c8a42a874981c78a32f1b0e77661327d9
FarhanJiddan <90674978+FarhanJiddan@users.noreply.github.com>
2021-09-14 15:08:05 +0700

    Add files via upload

---
+++ Contoh1.php
@@ -0,0 +1,12 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+  public function index()
+  {
+    echo "<h1>Perkenalkan</h1>";
+    echo "Nama saya Farhan Jiddan
+          saya tinggal di daerah Lubang buaya
+          olahraga yang saya sukai adalah bulutangkis";
+  }
+}
+ ?>
